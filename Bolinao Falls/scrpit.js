812a6019db12dfe598fa87a2a5c170097e1cc15e
Mayflay <basanez.jefferson@gmail.com>
2025-12-07 02:09:04 +0800

    Add files via upload

---
+++ Bolinao Falls/scrpit.js
@@ -0,0 +1,15 @@
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible'); // triggers slide-in
+        }
+      });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => observer.observe(section));
+  });
+</script>
